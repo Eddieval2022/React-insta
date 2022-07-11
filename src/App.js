33d5e7720cd786e1cd41336bf85999297627fd92
Edward Valentine <eddieval2022@gmail.com>
2022-07-11 22:03:54 +0100

    Render on click

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import {useState} from "react";
+
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+  const [photos, setPhotos] = useState([]);
+
+  const fetchImages = async () =>{
+    const response = await fetch("https://picsum.photos/v2/list");
+    const data = await response.json();
+    setPhotos(data);
+  }
+
+  return (
+    
+    <div className="App">
+      <h1>Test</h1>
+     <button onClick={(e)=> fetchImages()}>Fetch photos from LoremPicsum</button>
+     
+     {photos.map((item, index)=>{
+      return<div>
+        <img src={item.download_url} />
+     </div>
+     })}
+
+
+    </div>
+       );
+}
 export default App;
---
+++ src/Box.js
@@ -0,0 +1,9 @@
+const Box = (props) => {
+  return (
+    <div>
+      <h1>{props.item}</h1>
+    </div>
+  );
+};
+
+export default Box;
